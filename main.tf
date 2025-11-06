66d6cfdf703198d2c656cbbfff395a9720c81451
deekshashrestha <deekshashrestha123@gmail.com>
2025-11-06 12:55:32 +0545

    Add Terraform web server + CloudFront

--- main.tf
+++ main.tf
@@ -2,38 +2,32 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+# Generate SSH key automatically (works in CI/CD)
+resource "tls_private_key" "web" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
 resource "aws_key_pair" "web_key" {
   key_name   = "web-key"
-  public_key = file("C:/Users/diksha.Shrestha/.ssh/id_rsa.pub")
+  public_key = tls_private_key.web.public_key_openssh
 }
 
-resource "aws_instance" "web" {
-  ami           = "ami-0e53db6c5f29a338b"
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.web_key.key_name
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
-
-  user_data = file("user-data.sh")
-
-  tags = {
-    Name = "SimpleWebServer"
-  }
-}
-
+# Security Group
 resource "aws_security_group" "web_sg" {
   name        = "web_sg"
   description = "Allow HTTP and SSH"
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -43,5 +37,56 @@ resource "aws_security_group" "web_sg" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "web" {
+  ami                    = "ami-0e53db6c5f29a338b"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.web_key.key_name
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  user_data = file("user-data.sh")
+
+  tags = {
+    Name = "SimpleWebServer"
+  }
+}
+
+# CloudFront Distribution
+resource "aws_cloudfront_distribution" "web_distribution" {
+  enabled = true
+
+  origins {
+    domain_name = aws_instance.web.public_dns
+    origin_id   = "ec2-web-origin"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "ec2-web-origin"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  price_class = "PriceClass_200"
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
   }
 }
--- output.tf
+++ output.tf
@@ -5,3 +5,9 @@ output "public_ip" {
 output "cloudfront_domain" {
   value = aws_cloudfront_distribution.web_distribution.domain_name
 }
+
+output "private_key_pem" {
+  value     = tls_private_key.web.private_key_pem
+  sensitive = true
+}
+
